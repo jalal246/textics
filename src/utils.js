0d85e701ef01bb6e6b469a1033b321dd7a970f83
jalal246 <jimmy002020@gmail.com>
2020-03-09 17:39:30 +0200

    Add more useability to utils funcs

--- src/utils.js
+++ src/utils.js
@@ -1,29 +1,35 @@
 /**
+ * Replaces \r or \r\n with \n. Which enables us to deal with unique new line
+ * character.
+ *
+ * Note:
  * \r = CR (Carriage Return) → Used as a new line character in Mac OS before X
  * \n = LF (Line Feed) → Used as a new line character in Unix/Mac OS X
  * \r\n = CR + LF → Used as a new line character in Windows
  *
- * It replaces \r or \r\n with \n. Which enable us to deal with unique new line
- * character.
- *
  * @see {https://stackoverflow.com/a/15433225/6348157}
- * @param {string} str
+ * @param {string} [str=""]
+ * @param {RegExp} [LF="\n"]
  * @returns {string} modified with str with \n as new line
  */
-function unifyNewLineChar(str) {
-  return /\r|\r\n/.test(str) ? str.replace(/\r|\r\n/g, "\n") : str;
+function unifyNewLineChar(str = "", LF = "\n") {
+  const re = /\r|\r\n/g;
+  const unified = LF;
+
+  return re.test(str) ? str.replace(re, unified) : str;
 }
 
 /**
  * Checks string, if there is spaces in new lines, it removes it.
  *
- * Note: new line defined as: \n
- *
- * @param {string} str
- * @returns {string} -without spaces in new lines
+ * @param {string} [str=""]
+ * @param {RegExp} [replaceWith="\n"] - LF new line.
+ * @returns
  */
-function removeSpacesFromStart(str) {
-  return /\n\s+/.test(str) ? str.replace(/\n\s+/g, "\n") : str;
+function removeSpacesFromStart(str = "", replaceWith = "\n") {
+  const re = /\n\s+/g;
+
+  return re.test(str) ? str.replace(re, replaceWith) : str;
 }
 
 /**
@@ -31,11 +37,11 @@ function removeSpacesFromStart(str) {
  * Splits each line of given string to array element.
  *
  * @param {string} str
- * @param {*} [basedOn=/\n/] - new line defined as: \n
+ * @param {RegExp} [basedOn=/\n/] - LF new line.
  * @returns
  */
 function toArray(str, basedOn = /\n/) {
-  return str.trim().split(basedOn);
+  return str.split(basedOn);
 }
 
 module.exports = {

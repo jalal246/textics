4e72b47595cf6006c92a4125f7515271e21f0ec8
jalal246 <jimmy002020@gmail.com>
2020-03-09 18:23:49 +0200

    Add base function to utils

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,17 @@
+const LF = /\n/;
+
+/**
+ * Checks string, if there is spaces in new lines, it removes it.
+ *
+ * @param {string} [str=""]
+ * @param {RegExp} reg
+ * @param {RegExp} expression
+ * @returns {string} - modified replaced with expression
+ */
+function replaceWith(str = "", reg, expression) {
+  return reg.test(str) ? str.replace(reg, expression) : str;
+}
+
 /**
  * Replaces \r or \r\n with \n. Which enables us to deal with unique new line
  * character.
@@ -9,27 +23,26 @@
  *
  * @see {https://stackoverflow.com/a/15433225/6348157}
  * @param {string} [str=""]
- * @param {RegExp} [LF="\n"]
+ * @param {RegExp} [newLineExp=LF] -  LF new line.
  * @returns {string} modified with str with \n as new line
  */
-function unifyNewLineChar(str = "", LF = "\n") {
-  const re = /\r|\r\n/g;
-  const unified = LF;
+function unifyNewLineChar(str = "", newLineExp = LF) {
+  const reg = /\r|\r\n/g;
 
-  return re.test(str) ? str.replace(re, unified) : str;
+  return replaceWith(str, reg, newLineExp);
 }
 
 /**
  * Checks string, if there is spaces in new lines, it removes it.
  *
  * @param {string} [str=""]
- * @param {RegExp} [replaceWith="\n"] - LF new line.
- * @returns
+ * @param {*} [newLineExp=LF] - LF new line.
+ * @returns {string} modified with str with \n as new line
  */
-function removeSpacesFromStart(str = "", replaceWith = "\n") {
-  const re = /\n\s+/g;
+function replaceSpacesFromStart(str = "", newLineExp = LF) {
+  const reg = /\n\s+/g;
 
-  return re.test(str) ? str.replace(re, replaceWith) : str;
+  return replaceWith(str, reg, newLineExp);
 }
 
 /**
@@ -37,15 +50,15 @@ function removeSpacesFromStart(str = "", replaceWith = "\n") {
  * Splits each line of given string to array element.
  *
  * @param {string} str
- * @param {RegExp} [basedOn=/\n/] - LF new line.
+ * @param {RegExp} [newLineExp=LF] - LF new line.
  * @returns
  */
-function toArray(str, basedOn = /\n/) {
-  return str.split(basedOn);
+function toArray(str, newLineExp = LF) {
+  return str.split(newLineExp);
 }
 
 module.exports = {
   unifyNewLineChar,
-  removeSpacesFromStart,
+  replaceSpacesFromStart,
   toArray
 };

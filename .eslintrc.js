7508ea9b37ab3e601c2205e1e10ee84a68a2fc74
jalal246 <jimmy002020@gmail.com>
2020-12-12 01:00:40 +0200

    Add plugin to lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,12 @@
 module.exports = {
   env: {
     browser: true,
+    commonjs: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ["airbnb-base", "prettier"],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
   },
-  rules: {
-  },
+  rules: {},
 };
